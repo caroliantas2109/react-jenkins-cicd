b3fee4ee26775bb0ac8df7727c142fbf42ae69b8
Heather <heather.hb@hotmail.com>
2026-03-30 11:53:29 -0600

    Skeleton code
    created the initial code to start project.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Jenkins CI/CD</h1>
+        <p>A simple React app with a Jenkins CI/CD pipeline.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
